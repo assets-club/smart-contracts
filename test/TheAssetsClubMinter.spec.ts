fac37dcd8ec266218fb2f790a0582a53b09759da
Mathieu Bour <mathieu.tin.bour@gmail.com>
2023-01-29 05:21:19 +0100

    test: add TheAssetsClubMinter.getPrice tests

--- test/TheAssetsClubMinter.spec.ts
+++ test/TheAssetsClubMinter.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { BigNumber } from 'ethers';
+import { BigNumberish, utils } from 'ethers';
 import { ethers } from 'hardhat';
 import { faker } from '@faker-js/faker';
 import { loadFixture, setBalance } from '@nomicfoundation/hardhat-network-helpers';
@@ -25,8 +25,8 @@ describe('TheAssetsClubMinter', () => {
 
   let now: number;
 
-  let PRIVATE_SALE_PRICE: BigNumber;
-  let PUBLIC_SALE_PRICE: BigNumber;
+  const PRIVATE_SALE_PRICE = utils.parseEther('0.05');
+  const PUBLIC_SALE_PRICE = utils.parseEther('0.07');
   let WAITLIST_DURATION: number;
   let OG_DURATION: number;
 
@@ -34,8 +34,6 @@ describe('TheAssetsClubMinter', () => {
     ({ admin, user1, user2, user3 } = await getTestAccounts());
     ({ TheAssetsClub, TheAssetsClubMinter, config } = await loadFixture(stackFixture));
 
-    PRIVATE_SALE_PRICE = await TheAssetsClubMinter.OG_DURATION();
-    PUBLIC_SALE_PRICE = await TheAssetsClubMinter.PUBLIC_SALE_PRICE();
     WAITLIST_DURATION = (await TheAssetsClubMinter.WAITLIST_DURATION()).toNumber();
     OG_DURATION = (await TheAssetsClubMinter.OG_DURATION()).toNumber();
 
@@ -127,5 +125,45 @@ describe('TheAssetsClubMinter', () => {
       await increaseTo(now + OG_DURATION + WAITLIST_DURATION + faker.datatype.number(WAITLIST_DURATION));
       expect(await TheAssetsClubMinter.getTier()).to.equal(Tier.PUBLIC);
     });
+  });
+
+  describe('getPrice', () => {
+    it('should revert if quantity is zero', async () => {
+      await expect(TheAssetsClubMinter.getPrice(Tier.PUBLIC, 0))
+        .to.be.revertedWithCustomError(TheAssetsClubMinter, 'InvalidQuantity')
+        .withArgs(0);
+    });
+
+    it('should revert if quantity is greater than 3', async () => {
+      const invalidQuantity = 4 + faker.datatype.number();
+      await expect(TheAssetsClubMinter.getPrice(Tier.PUBLIC, invalidQuantity))
+        .to.be.revertedWithCustomError(TheAssetsClubMinter, 'InvalidQuantity')
+        .withArgs(invalidQuantity);
+    });
+
+    it('should revert if tier is locked', async () => {
+      await expect(TheAssetsClubMinter.getPrice(Tier.LOCKED, 1))
+        .to.be.revertedWithCustomError(TheAssetsClubMinter, 'Locked')
+        .withArgs();
+    });
+
+    const dataset: [Tier, number, BigNumberish][] = [
+      [Tier.OG, 1, 0],
+      [Tier.OG, 2, 0],
+      [Tier.OG, 3, PRIVATE_SALE_PRICE],
+      [Tier.WAITLIST, 1, 0],
+      [Tier.WAITLIST, 2, PRIVATE_SALE_PRICE],
+      [Tier.WAITLIST, 3, PRIVATE_SALE_PRICE.mul(2)],
+      [Tier.PUBLIC, 1, PUBLIC_SALE_PRICE],
+      [Tier.PUBLIC, 2, PUBLIC_SALE_PRICE.mul(2)],
+      [Tier.PUBLIC, 3, PUBLIC_SALE_PRICE.mul(3)],
+    ];
+
+    for (const [tier, quantity, expectedPrice] of dataset) {
+      const fPrice = utils.formatEther(expectedPrice);
+      it(`should compute price for (tier=${tier},quantity=${quantity}) => ${fPrice}\u039e`, async () => {
+        expect(await TheAssetsClubMinter.getPrice(tier, quantity)).to.equal(expectedPrice);
+      });
+    }
   });
 });
