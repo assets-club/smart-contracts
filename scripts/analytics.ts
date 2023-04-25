fbd30f2b92e55fe4fdbb4bf780025ec90d2f68a8
Mathieu Bour <mathieu@bour.tech>
2023-04-26 01:44:42 +0200

    feat: add analytics script

---
+++ scripts/analytics.ts
@@ -0,0 +1,67 @@
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+import { z } from 'zod';
+
+const COLLECTION_PATH = join(__dirname, '../collection');
+const metadataSchema = z.object({
+  attributes: z.array(
+    z.object({
+      trait_type: z.string().min(1),
+      value: z.string().min(1),
+    }),
+  ),
+});
+
+/**
+ * This scripts parses the generated metadatas and outputs repartitions in STDOUT.
+ */
+export default async function main() {
+  const stats: Record<string, Record<string, number>> = {};
+
+  for (let i = 0; i <= 5776; i++) {
+    const raw = await readFile(join(COLLECTION_PATH, `${i}.json`), { encoding: 'utf-8' });
+    const parsed = JSON.parse(raw) as Record<string, any>;
+
+    if ('attribute' in parsed) {
+      parsed.attributes = parsed.attribute;
+      delete parsed.attribute;
+    }
+
+    const data = await metadataSchema.parseAsync(parsed).catch((err) => {
+      console.log('err', i);
+      throw err;
+    });
+
+    for (const raw of data.attributes) {
+      const trait_type = raw.trait_type.toLowerCase().trim();
+      const value = raw.value.trim();
+
+      if (!(trait_type in stats)) {
+        stats[trait_type] = {};
+      }
+
+      if (!(value in stats[trait_type])) {
+        stats[trait_type][value] = 0;
+      }
+
+      stats[trait_type][value]++;
+    }
+  }
+
+  for (const [trait_type, data] of Object.entries(stats)) {
+    console.log('Trait type:', trait_type);
+    const table = Object.entries(data).map(([label, count]) => {
+      return {
+        label,
+        count,
+        percentage: ((count * 100) / 5777).toFixed(3) + '%',
+      };
+    });
+    console.table(table);
+  }
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exitCode = 1;
+});
